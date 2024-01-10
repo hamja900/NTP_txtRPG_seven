8c454bb6814342b68f95c809486bef7f44be5bb1
hamja900 <luru@naver.com>
2024-01-10 16:39:13 +0900

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace NTP_txtRPG_seven
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
